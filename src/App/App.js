3b1936670a4a2f3fe53faca47681cd1def484e78
ayminahmedpk <ayminahmedpk@gmail.com>
2022-06-25 23:21:01 +0500

    0.1.9

--- src/App/App.js
+++ src/App/App.js
@@ -2,16 +2,13 @@
 
 import React, { Component } from 'react'
 
-import Grandparent from '../Lessons/02-pure-and-memo/02-nesting-children-outside-jsx/Grandparent';
-import Parent from '../Lessons/02-pure-and-memo/02-nesting-children-outside-jsx/Parent';
-import Child from '../Lessons/02-pure-and-memo/02-nesting-children-outside-jsx/Child';
-import Grandchild from '../Lessons/02-pure-and-memo/02-nesting-children-outside-jsx/Grandchild';
+import Parent from '../Lessons/01-basics/02-child-components/02-child-components-props/Parent';
 
 class App extends Component {
   render() {
     return (
       <>
-        <Grandparent/>
+        <Parent/>
       </>
     )
   }
--- src/Lessons/01-basics/02-child-components/02-child-components-props/Parent.js
+++ src/Lessons/01-basics/02-child-components/02-child-components-props/Parent.js
@@ -8,14 +8,24 @@ export default class Parent extends Component {
   counter = 0;
   incrementCounter = () => {this.counter++; console.log('click happened'); };
   decrementCounter = () => {this.counter--; console.log('click happened'); };
+  
+  
+  // Force update makes it work.
+  // It really needs a state change or a forced update to make react actually care about props
+
+  // incrementCounter = () => {this.counter++; console.log('click happened'); this.forceUpdate();};
+  // decrementCounter = () => {this.counter--; console.log('click happened'); this.forceUpdate();};
+
+  // Try using one of these and one of the above together.
+  // You'll see that count changes properly, but update isn't triggered. Man.
   render() {
     return (
       <>
         <div className='component parent'>
           <p>Parent</p>
           <p>Count: {this.counter}</p>
-          <button onClick={this.decrementCounter} >Decrement Counter</button>
-          <button onClick={this.incrementCounter} >Increment Counter</button>
+          <button onClick={this.decrementCounter}>Decrement Counter</button>
+          <button onClick={this.incrementCounter}>Increment Counter</button>
           <Child
             sampleProp       = "Sample Prop"
             counter          = {this.counter}
